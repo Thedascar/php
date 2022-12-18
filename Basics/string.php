fe0fe323773e55abcd96d8d6a1ac3e6bef6a1005
lucas <lcs_demais@hotmail.com>
2022-12-17 21:44:49 -0300

    att

--- Basics/string.php
+++ Basics/string.php
@@ -4,8 +4,14 @@ $nome = "Lucas";
 $sobrenome = "Ramon";
 $idade = 27;
 /**
- * \n para pular a linha
+ * \n <<<<< para pular a linha
+ * sempre usar "" aspas duplas
+ * se usar '' linhas simples não terá efeito
+ * 
+ * \t <<< tab ma linha
  */
-echo "Óla Mundo\n";
-
+/**
+ * .PHP_EOL <<<< adiciona a linha , mas com uma forma do php.
+ */
+echo "Óla Mundo" . PHP_EOL;
 echo "Meu nome é $nome $sobrenome e minha idade é $idade ";

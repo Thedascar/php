a251de7115863ffef80e1a6886b63e05854e14a3
lucas <lcs_demais@hotmail.com>
2022-12-17 21:38:34 -0300

    aat

--- Basics/string.php
+++ Basics/string.php
@@ -3,4 +3,9 @@
 $nome = "Lucas";
 $sobrenome = "Ramon";
 $idade = 27;
-echo "Meu nome é $nome $sobrenome e minha idade é $idade "; 
+/**
+ * \n para pular a linha
+ */
+echo "Óla Mundo\n";
+
+echo "Meu nome é $nome $sobrenome e minha idade é $idade ";

5dd67f7a94423878ea64acfb436aefb19e905af3
lucas <lcs_demais@hotmail.com>
2022-12-20 23:11:26 -0300

    att

---
+++ desafios/desafio-tabuada.php
@@ -0,0 +1,8 @@
+<?php
+
+$seletor = 9;
+
+for ($i= 0 ; $i <= 10 ; $i++) { 
+    $resultado = $seletor * $i;
+    echo $seletor."X".$i."=".$resultado . PHP_EOL;
+}
---
+++ desafios/imc.php
@@ -0,0 +1,17 @@
+<?php
+
+$peso = 110;
+$altura = 1.65;
+$imc = $peso / ($altura ** 2);
+
+if($imc < 18.5){
+    echo "seu imc é $imc e está desnutrido";
+}elseif($imc > 18.5 && $imc < 24.9){
+    echo "seu imc é $imc e está normal";
+}elseif($imc > 25.0 && $imc  < 29.9){
+    echo "seu imc é $imc e está com sobrepeso";
+}elseif($imc > 30.0 && $imc < 39.9){
+    echo "seu imc é $imc e está obesidade";
+}else{
+    echo "seu imc é $imc e está obesidade grave";
+}

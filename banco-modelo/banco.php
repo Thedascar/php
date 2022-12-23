7e28a0c13fd907b303a9d6fbeb23da580bcac9d1
lucas <lcs_demais@hotmail.com>
2022-12-23 10:46:14 -0300

    att

--- banco-modelo/banco.php
+++ banco-modelo/banco.php
@@ -1,12 +1,20 @@
 <?php
 
 /**
+ * Criação de uma função com uma sub-rotina de exibir as mensagens.
+ */
+function exibeMensagem($mensagem){
+    echo $mensagem . PHP_EOL;
+}
+
+
+/**
  * Modificando os indices dos arrays.
  */
 $contasCorrentes = [
     "123.456.787-10" => ["titular" => "Lucas","saldo" => 1000],
     "123.456.788-11" =>["titular" => "Aline","saldo" => 15000],
-    "123.456.789-12" =>["titular" => "Bacon e Bob","saldo" => 400]
+    "123.456.789-12" =>["titular" => "Bacon e Bob","saldo" => 500]
 ];
 
 /**
@@ -14,7 +22,7 @@ $contasCorrentes = [
 */
 
 if(500 > $contasCorrentes["123.456.787-10"]['saldo'] ){
-    echo "Você não pode sacar este valor " . PHP_EOL;
+    exibeMensagem("Você não pode sacar este valor ");
 }else{
     $contasCorrentes["123.456.787-10"]['saldo'] -= 500;
 }
@@ -22,12 +30,12 @@ if(500 > $contasCorrentes["123.456.787-10"]['saldo'] ){
 
 
 if(500 > $contasCorrentes["123.456.789-12"]['saldo'] ){
-    echo "Você não pode sacar este valor " . PHP_EOL;
-}else{
-$contasCorrentes["123.456.789-12"]['saldo'] -= 500;
+    exibeMensagem("Você não pode sacar este valor ");
+}else{  
+    $contasCorrentes["123.456.789-12"]['saldo'] -= 400;
 }
 
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " .$conta["titular"]."tem saldo de ".$conta["saldo"] . PHP_EOL;
+    exibeMensagem($cpf . " " .$conta["titular"]."tem saldo de ".$conta["saldo"]);
 }

4ae5865d86d6d35df0bac9679d4c7bd320a63366
lucas <lcs_demais@hotmail.com>
2022-12-28 06:57:20 -0300

    att

---
+++ arrays/validando-array.php
@@ -0,0 +1,43 @@
+<?php
+
+$notas = [
+    'Lucas' => 10,
+    'Aline' => 8,
+    'Bob' => 9,
+    'Bacon' => 7,
+    'nimguem' => 6
+];
+
+ksort($notas);
+var_dump($notas);
+
+/**
+ * Função is_array nos ajudara validar um array
+ * mostrando se a lista é um array ou não.
+ */
+if (is_array($notas)){
+    echo 'É um array';   
+
+}
+
+var_dump(array_is_list($notas));
+
+
+
+$arrays = [
+   0 => 'um',
+   1 => 'dois',
+   3 => 'tres'    
+];
+
+foreach ($arrays as $key => $nomeNumero) {
+    echo "$key em português é: $nomeNumero" . PHP_EOL;
+}
+
+echo "Total: " . count($arrays). PHP_EOL;
+
+/**
+ * Arrays_is_list faz uma verificação para ver se o arrays e uma lista
+ * sequencial para poder fazer um for.
+ */
+var_dump(array_is_list($arrays));
